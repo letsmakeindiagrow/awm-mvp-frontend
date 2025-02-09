ce4a2a66cf7ab8925cdc349ee74b0ccdfea73534
Pranay <sharmapranay38@gmail.com>
2025-02-09 15:59:58 +0530

    added new blobs and made them as told by atul sir

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -7,11 +7,13 @@ import { Testimonials } from "@/components/testimonials";
 import { CTA } from "@/components/cta";
 import { Footer } from "@/components/footer";
 import BlobBackground from "@/components/BlobBackground"; // ✅ Import the Blob Background
+import BlobStatic from "@/components/BlobStatic";
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-white">
-      <BlobBackground /> {/* ✅ Background added here */}
-      <Navbar />
+      {/* <BlobBackground /> */}
+      <BlobStatic />
+       <Navbar />
       <Hero />
       <Features />
       <BlogSection />
--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -1,10 +1,13 @@
 import { Navbar } from "@/components/navbar";
 import { Footer } from "@/components/footer";
 import RegistrationForm from "@/components/RegistrationForm";
+import BlobBackground from "@/components/BlobBackground";
+import BlobBackgroundOther from "@/components/BlobBackgroundOther";
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-white">
       {/* <Navbar /> */}
+      <BlobBackgroundOther />
       <RegistrationForm />
       {/* <Footer /> */}
     </div>
