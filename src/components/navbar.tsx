3169d3b5ac1829271346ca8446481df324523785
Pranay <sharmapranay38@gmail.com>
2025-02-09 11:13:09 +0530

    fixed logo link

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -24,7 +24,7 @@ export function Navbar() {
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between h-20">
           <div className="flex-shrink-0 flex items-center">
-            <Link to="/">
+            <Link to="/" onClick={()=>window.scrollTo(0,0)}>
               <img
                 src="/new logo png website.png"
                 alt="Aadyanvi Wealth"
