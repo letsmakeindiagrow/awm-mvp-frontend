e17d19ce89f14802fe169fa6e94002c88a36334e
Pranay <sharmapranay38@gmail.com>
2025-02-10 21:02:54 +0530

    added otp for phone and email

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -3,11 +3,13 @@ import { Footer } from "@/components/footer";
 import RegistrationForm from "@/components/RegistrationForm";
 import BlobBackground from "@/components/BlobBackground";
 import BlobBackgroundOther from "@/components/BlobBackgroundOther";
+import TempRegistration from "@/components/TempRegistration";
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-white">
       {/* <Navbar /> */}
-      <RegistrationForm />
+      {/* <RegistrationForm /> */}
+      <TempRegistration />
       {/* <Footer /> */}
     </div>
   );
