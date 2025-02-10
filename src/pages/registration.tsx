8f2ccab5177db1a8df92321e56fea79f10b53ccd
pushkar1713 <pushkar1713@gmail.com>
2025-02-10 21:25:19 +0530

    linting errors from pranay2

--- src/pages/registration.tsx
+++ src/pages/registration.tsx
@@ -1,16 +1,8 @@
-import { Navbar } from "@/components/navbar";
-import { Footer } from "@/components/footer";
-import RegistrationForm from "@/components/RegistrationForm";
-import BlobBackground from "@/components/BlobBackground";
-import BlobBackgroundOther from "@/components/BlobBackgroundOther";
 import TempRegistration from "@/components/TempRegistration";
 export default function Home() {
   return (
     <div className="relative min-h-screen bg-white">
-      {/* <Navbar /> */}
-      {/* <RegistrationForm /> */}
       <TempRegistration />
-      {/* <Footer /> */}
     </div>
   );
 }
